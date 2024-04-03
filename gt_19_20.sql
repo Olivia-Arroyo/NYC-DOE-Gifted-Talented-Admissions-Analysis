b9fc3436b6d9187924e813cb4c46654ed8685367
Olivia Arroyo <128095011+Olivia-Arroyo@users.noreply.github.com>
2024-04-02 21:04:30 -0400

    Update gt_19_20.sql

--- gt_19_20.sql
+++ gt_19_20.sql
@@ -33,13 +33,13 @@ MODIFY COLUMN Offers INT;
 CREATE TABLE gt_19_20_clean
 AS
 SELECT 
-	Year,
+    Year,
     District,
-	Tested,
+    Tested,
     Eligible,
     (Eligible / Tested * 100) AS Perecent_Eligible_After_Testing,
     Applicants,
-	(Applicants / Eligible * 100) AS Percent_Applicants_From_Eligible_Pool,
+    (Applicants / Eligible * 100) AS Percent_Applicants_From_Eligible_Pool,
     Offers,
     (Offers / Applicants * 100) AS Percent_Offers_Made_Applicants_Pre_Changes
 FROM gt_results_19_20
